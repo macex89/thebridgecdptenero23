f6d8cf8e7b3a34785b711f05fd53d2a9596f0dd4
isotomor <i.sotomoreno@gmail.com>
2023-02-11 11:27:29 +0100

    Actualizamos..

--- 01 - RampUp/Modulo 3 - Fundamentos de Administración de sistemas/CMD/Ejercicios/Solucion/ejercicio1.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Ejercicio2
-SETLOCAL enabledelayedexpansion
-set /p tabladel="Dime de que numero quieres la tabla: "
-
-for /l %%n in (1,1,10) do (
-    set /a valor= %tabladel% * %%n
-    echo %tabladel% * %%n = !valor!
-)
--- 01 - RampUp/Modulo 3 - Fundamentos de Administración de sistemas/CMD/Ejercicios/Solucion/ejercicio3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p cant="Introduce an amount: "
-for %%f in (*) do (
-    if %%~zf GTR %cant% echo %%f - %%~zf bytes
-)
